470bfe06c9c83ce1cb8ec5963d5d752d210d5e12
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-10-18 04:35:16 +0700

    backend

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,58 @@
+import UserModel from "../models/UserModel.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const response = await UserModel.findAll();
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const response = await UserModel.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+    await UserModel.create(req.body);
+    res.status(201).json({ msg: "User Created" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    await UserModel.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: "User Updated" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    await UserModel.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json({ msg: "User Deleted" });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
